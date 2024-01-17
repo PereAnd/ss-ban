ba8c5efaf4e8f8e67b278e863ef813ee62b77d4a
alvaroperez <alvaroperez@cbit-online.com>
2024-01-17 16:44:45 -0500

    Se agregann los endpoints para la intención de compra

--- app.js
+++ app.js
@@ -13,18 +13,19 @@ const baseUrl = "https://gw-sandbox-qa.apps.ambientesbc.com/public-partner";
 
 app.post("/tokenBancolombia", async (req, res) => {
   let grant_type = req.body.grant_type;
-  let scope = "Product-balance:read:user TermsConditions:read:user TermsConditions-register:write:user";
+  let scope =
+    "Product-balance:read:user TermsConditions:read:user TermsConditions-register:write:user Transfer-Intention:read:app Transfer-Intention:write:app";
   let client_id = req.body.client_id;
   let client_secret = req.body.client_secret;
   let auth64 = btoa(`${client_id}:${client_secret}`);
 
   console.log({
-    'endpoint': '/tokenBancolombia',
-    'timestamp': new Date(),
-    'grant_type': grant_type,
-    'client_id': client_id,
-    'client_secret': client_secret
-  })
+    endpoint: "/tokenBancolombia",
+    timestamp: new Date(),
+    grant_type: grant_type,
+    client_id: client_id,
+    client_secret: client_secret,
+  });
 
   let url = baseUrl + "/sb//security/oauth-provider/oauth2/token";
   const formData = new URLSearchParams();
@@ -57,10 +58,10 @@ app.post("/tyc", async (req, res) => {
   };
 
   console.log({
-    'endpoint': '/tyc',
-    'timestamp': new Date(),
-    'access_token': access_token
-  })
+    endpoint: "/tyc",
+    timestamp: new Date(),
+    access_token: access_token,
+  });
 
   let url =
     baseUrl +
@@ -81,36 +82,36 @@ app.post("/aceptacionTyC", async (req, res) => {
   let aceptacion = req.body.aceptacion;
 
   console.log({
-    'endpoint': '/aceptacionTyC',
-    'timestamp': new Date(),
-    'access_token': access_token,
-    'aceptacion': aceptacion
-  })
+    endpoint: "/aceptacionTyC",
+    timestamp: new Date(),
+    access_token: access_token,
+    aceptacion: aceptacion,
+  });
   let headers = {
     sessionToken: "test",
     messageId: "c4e6bd04-5149-11e7-b114-b2f933d5fe66",
     "Content-Type": "application/vnd.bancolombia.v4+json",
-    "Accept": "application/vnd.bancolombia.v4+json",
-    Authorization: "Bearer " + access_token
+    Accept: "application/vnd.bancolombia.v4+json",
+    Authorization: "Bearer " + access_token,
   };
 
   let data = {
-    "data": {
-      "security": {
-        "enrollmentKey": "f"
+    data: {
+      security: {
+        enrollmentKey: "f",
       },
-      "termsCondition": {
-        "clausesCustomer": {
-          "acceptance": aceptacion,
-          "version": "1"
+      termsCondition: {
+        clausesCustomer: {
+          acceptance: aceptacion,
+          version: "1",
         },
-        "walletTerms": {
-          "acceptance": aceptacion,
-          "version": "1"
-        }
-      }
-    }
-  }
+        walletTerms: {
+          acceptance: aceptacion,
+          version: "1",
+        },
+      },
+    },
+  };
 
   let url =
     baseUrl +
@@ -131,10 +132,10 @@ app.post("/saldoCuenta", async (req, res) => {
   let access_token = req.body.access_token;
 
   console.log({
-    'endpoint': '/saldoCuenta',
-    'timestamp': new Date(),
-    'access_token': access_token
-  })
+    endpoint: "/saldoCuenta",
+    timestamp: new Date(),
+    access_token: access_token,
+  });
   let url =
     baseUrl +
     "/sb//v1/operations/product-specific/consumer-services/brokered-product/bancolombiapay-wallet-information/wallet-balance/retrieveBalance";
@@ -168,6 +169,83 @@ app.post("/saldoCuenta", async (req, res) => {
 
   const responseData = await response.json();
 
+  res.json(responseData);
+});
+
+app.post("/intencionCompra", async (req, res) => {
+  let access_token = req.headers.access_token;
+  let valorCompra = req.body.valorCompra;
+
+  console.log({
+    endpoint: "/intencionCompra",
+    timestamp: new Date(),
+    access_token: access_token,
+    valorCompra: valorCompra,
+  });
+
+  const headers = {
+    "Content-Type": "application/vnd.bancolombia.v4+json",
+    Accept: "application/vnd.bancolombia.v4+json",
+    Authorization: "Bearer " + access_token,
+  };
+
+  let data = {
+    data: [
+      {
+        commerceTransferButtonId: "h4ShG3NER1C",
+        transferReference: "9999999996",
+        transferAmount: valorCompra,
+        commerceUrl: "https://gateway.com/payment/route?commerce=Telovendo",
+        transferDescription: "Compra de productos Cbitbank",
+        confirmationURL:
+          "https://pagos-api-dev.tigocloud.net/bancolombia/callback",
+      },
+    ],
+  };
+
+  let url =
+    baseUrl +
+    "/sb/v3/operations/cross-product/payments/payment-order/transfer/action/registry";
+
+  const response = await fetch(url, {
+    method: "POST",
+    headers: headers,
+    body: JSON.stringify(data),
+  });
+
+  const responseData = await response.json();
+  console.log(responseData);
+
+  res.json(responseData);
+});
+
+app.post("/estadoCompra", async (req, res) => {
+  let access_token = req.headers.access_token;
+  let transferCode = req.body.transferCode;
+
+  console.log({
+    endpoint: "/estadoCompra",
+    timestamp: new Date(),
+    access_token: access_token,
+  });
+
+  const headers = {
+    "Content-Type": "application/vnd.bancolombia.v4+json",
+    Accept: "application/vnd.bancolombia.v4+json",
+    Authorization: "Bearer " + access_token,
+  };
+
+  let url =
+    baseUrl +
+    `/sb/v3/operations/cross-product/payments/payment-order/transfer/${transferCode}/action/validate`;
+
+  const response = await fetch(url, {
+    method: "GET",
+    headers: headers,
+  });
+
+  const responseData = await response.json();
+  console.log(responseData);
   res.json(responseData);
 });
 
